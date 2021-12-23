dda513a3e915ee6fb4a8fc17b286239dc3655b00
Kate2120 <gifos.kate@gmail.com>
2021-12-23 18:32:07 +0200

    first

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
 import Menu from './components/Menu/menu';
+import { useState } from 'react';
 
 function App() {
+    const [dish, setDish] = useState([{},{}]);
+
+    const handleClick = () => {
+      dish.filter()
+    }
   return (
     <div className="App">
       <Header/>
-      <Menu/>
+      <Menu onClick={}/>
+      
       <Footer/>
       
     </div>
