80a68832e3ae27b8a79c3b763cc8ecf944894fb3
Kate <gifos.kate@gmail.com>
2021-12-27 06:28:06 +0200

    отделила массивы с данными, удалила лишние пустые компоненты

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
 import Header from './components/Header/header';
 import Footer from './components/Footer/footer';
-import Menu from './components/Menu/menu';
 import Filter from "./components/Menu/menu";
 
-
-
 function App() {
-
   return (
     <div className="App">
       <Header/>
